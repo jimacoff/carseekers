892b1aa5047c8ff25f1d087f638480c4f476ce4b
Edgar Cumbreras <edgarcumbrerastirado@gmail.com>
2013-12-08 18:14:23 +0000

    Adds gravatar feature with helper tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def gravatar_picture_url(user, size=200)
+    if user.picture_url.present?
+      user.picture_url
+    else
+      default_url = "#{root_url}images/guest.png"
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{default_url}"
+    end
+  end
+
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe ApplicationHelper do
+  before do
+    @user = User.make!
+    @gravatar_id = Digest::MD5.hexdigest(@user.email.downcase)
+    @size = 200
+  end
+
+  it "Should return the correct avatar from Gravatar" do
+    gravatar_picture_url(@user, @size).should eq("http://gravatar.com/avatar/#{@gravatar_id}.png?s=#{@size}&d=#{root_url}images/guest.png")
+  end
+end
