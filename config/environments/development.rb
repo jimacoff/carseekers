5fe7bfbd4f2c2f251866f251c723b0340288f700
Edgar Cumbreras <edgarcumbrerastirado@gmail.com>
2013-12-12 19:03:18 +0000

    Adds mod to heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Carseekers::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.secret_key_base = 'blipblapblup'
 
   #Google Keys for Devise OAuth2
   config.google_app_id = "215933734228-r84fbefpidgvbaj6fb8ci6qk40ff4e9g.apps.googleusercontent.com"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carseekers::Application.config.secret_key_base = '2fcbb4c73e896ed0108cd9fa6dc5a13bcafa4cf6188a6cafa48dbffe03925746435598d48749eba47b725a26da77c3bb9ca0a1c25ff548d20d2e84782ab216eb'
