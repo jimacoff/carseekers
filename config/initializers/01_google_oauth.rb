caa8b9eb03a66bcececa58ed32aaff8c77d19b93
Edgar Cumbreras <edgarcumbrerastirado@gmail.com>
2013-12-12 19:43:14 +0000

    Adds google oauth file

--- config/initializers/01_google_oauth.rb
+++ config/initializers/01_google_oauth.rb
@@ -1,6 +1,11 @@
-google_oauth_secrets = YAML::load_file('config/google_oauth.yml')[Rails.env] if File.exist?('config/google_oauth.yml')
-
 Carseekers::Application.configure do
-  config.google_app_id = ENV["google_app_id"] || google_oauth_secrets["google_app_id"]
-  config.google_app_secret = ENV["google_app_secret"] || google_oauth_secrets["google_app_secret"]
+  if Rails.env == "production"
+    config.google_app_id = ENV["google_app_id"]
+    config.google_app_secret = ENV["google_app_secret"]
+  else
+    google_oauth_secrets = YAML::load_file('config/google_oauth.yml')[Rails.env]
+      config.google_app_id = ENV["google_app_id"] || google_oauth_secrets["google_app_id"]
+      config.google_app_secret = ENV["google_app_secret"] || google_oauth_secrets["google_app_secret"]
+  end
 end
+
