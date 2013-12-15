4293c2b00dc1b9b2df7a810b576c9776885f1f67
Edgar Cumbreras <edgarcumbrerastirado@gmail.com>
2013-12-15 10:18:07 +0000

    Improves test performance

--- features/step_definitions/delete_message_steps.rb
+++ features/step_definitions/delete_message_steps.rb
@@ -1,10 +1,8 @@
 When(/^he deletes a message$/) do
-  save_and_open_page
   click_link "Your Received Messages:"
   click_link "Delete"
 end
 
 Then(/^he should not see that message anymore$/) do
-  save_and_open_page
   expect(page).not_to have_content @message.subject
 end
