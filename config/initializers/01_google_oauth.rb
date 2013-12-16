0a811b6ef3baa24c84c19f612329c29cac141076
Edgar Cumbreras <edgarcumbrerastirado@gmail.com>
2013-12-16 10:12:12 +0000

    Modifies Google Auth2 file

--- config/initializers/01_google_oauth.rb
+++ config/initializers/01_google_oauth.rb
@@ -2,13 +2,10 @@ Carseekers::Application.configure do
   if Rails.env == "production"
     config.google_app_id = ENV["google_app_id"]
     config.google_app_secret = ENV["google_app_secret"]
-  elsif Rails.env == "development"
-    config.google_app_id = ENV["google_app_id"]
-    config.google_app_secret = ENV["google_app_secret"]
   else
     google_oauth_secrets = YAML::load_file('config/google_oauth.yml')[Rails.env]
-      config.google_app_id = ENV["google_app_id"] || google_oauth_secrets["google_app_id"]
-      config.google_app_secret = ENV["google_app_secret"] || google_oauth_secrets["google_app_secret"]
+    config.google_app_id = ENV["google_app_id"] || google_oauth_secrets["google_app_id"]
+    config.google_app_secret = ENV["google_app_secret"] || google_oauth_secrets["google_app_secret"]
   end
 end
 
